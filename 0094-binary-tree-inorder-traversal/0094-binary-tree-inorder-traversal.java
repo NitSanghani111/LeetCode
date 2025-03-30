6e07f72f95bbf5349fdc1d3c777c3dd021a611c4
Nit Patel <155795470+NitSanghani111@users.noreply.github.com>
2025-03-30 10:05:24 +0530

    Time: 0 ms (100%), Space: 41.5 MB (82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,20 +13,35 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         Stack<TreeNode> stack = new Stack<>();
+//         List<Integer> res = new ArrayList<>();
+//         TreeNode currentNode = root;
+//         while(currentNode !=null || !stack.isEmpty() ){
+//             while(currentNode !=null){
+//                 stack.push(currentNode);
+//                 currentNode =currentNode.left;
+//             }
+//             currentNode =stack.pop();
+//             res.add(currentNode.val);
+//             currentNode = currentNode.right;
+//         }
+//      return res;   
+//     }
+// }
+//aporch 2 date 30-3-25
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
+     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        TreeNode currentNode = root;
-        while(currentNode !=null || !stack.isEmpty() ){
-            while(currentNode !=null){
-                stack.push(currentNode);
-                currentNode =currentNode.left;
-            }
-            currentNode =stack.pop();
-            res.add(currentNode.val);
-            currentNode = currentNode.right;
-        }
-     return res;   
-    }
+        inorder(root,res);
+        return res;
+        
+     }
+     private void inorder(TreeNode root, List<Integer>res){
+        if(root == null)  return ;
+        inorder(root.left,res);
+        res.add(root.val);
+        inorder(root.right,res);
+     }
 }
