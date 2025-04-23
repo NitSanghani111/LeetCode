f69f31b1ca1152794a8e99b785a2b424574c62c0
Nit Patel <155795470+NitSanghani111@users.noreply.github.com>
2025-04-23 23:58:06 +0530

    Time: 5 ms (50.8%), Space: 43.5 MB (60.55%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String [] compo = path.split("/");
+        Stack<String> st = new Stack<>();
+        for(String comp : compo){
+            if(comp.equals("") || comp.equals(".")){
+                continue;
+            }
+            if(comp.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                
+            }
+            else{
+                st.push(comp);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.insert(0, "/" + st.pop());
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString(); 
+    }
+}
