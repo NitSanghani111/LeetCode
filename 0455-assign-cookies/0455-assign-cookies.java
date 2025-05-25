170f0b598b88b46bcca44a5ca704dc854dd0edcf
Nit Patel <155795470+NitSanghani111@users.noreply.github.com>
2025-05-25 22:09:06 +0530

    Time: 11 ms (39.88%), Space: 48.8 MB (24.65%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        if(s.length == 0) return 0;
         int count = 0;
         Arrays.sort(g);
         Arrays.sort(s);
